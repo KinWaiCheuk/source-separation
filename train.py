58d04b90a0084cb8721b07cfc294d881f19354b3
Kin Wai Cheuk <u3500684@connect.hku.hk>
2023-05-11 09:10:35 +0800

    added hydra support

--- train.py
+++ train.py
@@ -2,18 +2,26 @@ import torch
 import pytorch_lightning as pl
 import models as Model
 from AudioLoader.music.mss import Moisesdb23
+import hydra
+from hydra.utils import to_absolute_path
 
 import os
 
-trainset = Moisesdb23('/root/dataset/moisesdb23_labelnoise_v1.0_16k_stereo/', segment=10)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,shuffle=True, num_workers=8)
+
+@hydra.main(config_path="config", config_name="exp")
+def my_app(cfg):
+    cfg.data_root = to_absolute_path(cfg.data_root)
+    trainset = Moisesdb23(**cfg.dataset.train)
+    trainloader = torch.utils.data.DataLoader(trainset, **cfg.dataloader.train)
 
 
-model = getattr(Model, 'Conv128Spec')()
-print(f"{model=}")
+    model = getattr(Model, cfg.model_name)()
 
-trainer = pl.Trainer(max_epochs=1000, gpus=[1])
+    trainer = pl.Trainer(**cfg.trainer)
 
 
-trainer.fit(model, trainloader)
-# check if bin 0-20 has changed
+    trainer.fit(model, trainloader)
+    # check if bin 0-20 has changed
+    
+if __name__ == "__main__":
+    my_app()
