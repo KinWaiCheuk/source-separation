59e5f96bd2810c2f248cdde86c93a3bf61a2bcbd
Kin Wai Cheuk <u3500684@connect.hku.hk>
2023-05-16 15:46:06 +0800

    added diffusion version of the model

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .conv128 import Conv128
 from .conv128_spectral import Conv128Spec
 from .conv128_superres import Conv128SuperRes
+from .conv128_diff import Conv128Diff
---
+++ train_diff.py
@@ -0,0 +1,40 @@
+import torch
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from pytorch_lightning.loggers import TensorBoardLogger
+import models as Model
+from AudioLoader.music.mss import Moisesdb23
+import hydra
+from hydra.utils import to_absolute_path
+
+import os
+
+
+@hydra.main(config_path="config", config_name="diff")
+def my_app(cfg):
+    cfg.data_root = to_absolute_path(cfg.data_root)
+    trainset = Moisesdb23(**cfg.dataset.train)
+    valset = Moisesdb23(**cfg.dataset.train)
+    trainloader = torch.utils.data.DataLoader(trainset, **cfg.dataloader.train)
+    valloader = torch.utils.data.DataLoader(trainset, **cfg.dataloader.train)
+
+    model = getattr(Model, cfg.model_name)(**cfg.model)
+
+    checkpoint_callback = ModelCheckpoint(monitor="Train/mse_wav",
+                                          filename=f"{cfg.model_name}-" + "{epoch:02d}",
+                                          save_top_k=2,
+                                          mode="min",
+                                          auto_insert_metric_name=False,
+                                          save_last=True)
+    name = f"{cfg.model_name}-{cfg.sr}"
+    logger = TensorBoardLogger(save_dir=".", version=1, name=name)      
+    trainer = pl.Trainer(**cfg.trainer,
+                         callbacks=[checkpoint_callback,],
+                         logger=logger)
+
+
+    trainer.fit(model, trainloader, valloader)
+    # check if bin 0-20 has changed
+    
+if __name__ == "__main__":
+    my_app()
